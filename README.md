df16c563aac007f058cb83301a350fa4d13d344c
YourOrdinaryDev <pr@yourordinarydev.com>
2023-03-21 19:52:21 +0100

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# fun-with-vue
+# fun with vue :)
+
+## requirements
+- [node.js LTS](https://nodejs.org/en/download)
+
+## setup
+```shell
+npm install && npm run dev
+```
+
+visit http://localhost:5173
+
+## contribution
+- lint/code style
+```shell
+npm run lint
+npm run cs
+```
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+      outDir: "docs"
+  },
+  plugins: [
+      vue(),
+      VitePWA({
+        registerType: "autoUpdate",
+      })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
