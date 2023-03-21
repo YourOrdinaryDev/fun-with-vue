c9c759e7684905b5fb812048fdb8193a7ab18257
YourOrdinaryDev <pr@yourordinarydev.com>
2023-03-21 20:01:31 +0100

    initial setup for GH pages

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fun-with-vue/sw.js', { scope: '/fun-with-vue/' })})}
